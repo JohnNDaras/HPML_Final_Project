350f9223584fe3869ca34f56d4854f28bc0511be
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-12-23 02:44:45 +0200

    Update test.py

--- Dynamic_Similarity_Algorithm/test.py
+++ Dynamic_Similarity_Algorithm/test.py
@@ -5,5 +5,5 @@ x=0
 print('Enter desired recall:')
 x = input()
 
-sg = Dynamic_Similarity_Algorithm(budget=5631064, qClusters = 29548, avSimilarity= 70.20773707391128, delimiter='\t',  sourceFilePath= main_dir + "SourceDataset.csv", targetFilePath = main_dir + "TargetDataset.csv", testFilePath = main_dir + 'similarity_results.csv', target_recall = float(x), similarity_index_range = 0.1)
+sg = Dynamic_Similarity_Algorithm(budget=5631064, delimiter='\t',  sourceFilePath= main_dir + "SourceDataset.csv", targetFilePath = main_dir + "TargetDataset.csv", testFilePath = main_dir + 'similarity_results.csv', target_recall = float(x), similarity_index_range = 0.1)
 sg.applyProcessing()
